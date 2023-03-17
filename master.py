80240f675feae74449eb97ec8857b854e78c8689
thegreatlizzyator <barthelemy.elizabeth@gmail.com>
2023-03-17 12:46:54 +0100

    create introduction method for master

--- master.py
+++ master.py
@@ -1,3 +1,5 @@
+import pyfiglet
+
 from board import Board
 
 class DungeonMaster :
@@ -28,3 +30,14 @@ class DungeonMaster :
         pass
         # if condition de victoire -->  self.board 
         #   self.gameover = True
+    
+    def introduction(self):
+        ascii_banner = "\n" + "-"*73 + "\n"
+        ascii_banner += str(pyfiglet.figlet_format("OTHELLO", font= "starwars"))
+        ascii_banner += "-"*73 + "\n"
+        ascii_banner += " "*28 + "Alexandre Appolaire" + " "*20 +"\n"
+        ascii_banner += " "*29 + "Lizzy Barthelemy" + " "*20 +"\n"
+        ascii_banner += " "*32 + "Aline Cisse" + " "*20 +"\n"
+        ascii_banner += "-"*73
+        print(ascii_banner)
+
--- othello.py
+++ othello.py
@@ -1,10 +1,5 @@
-import pyfiglet
+from master import DungeonMaster
 
-ascii_banner = "\n" + "-"*73 + "\n"
-ascii_banner += str(pyfiglet.figlet_format("OTHELLO", font= "starwars"))
-ascii_banner += "-"*73 + "\n"
-ascii_banner += " "*28 + "Alexandre Appolaire" + " "*20 +"\n"
-ascii_banner += " "*29 + "Lizzy Barthelemy" + " "*20 +"\n"
-ascii_banner += " "*32 + "Aline Cisse" + " "*20 +"\n"
-ascii_banner += "-"*73
-print(ascii_banner)
+master = DungeonMaster()
+master.introduction()
+
