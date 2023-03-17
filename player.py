72c0e634256a9da15281c568ed4e115e10f22f2d
thegreatlizzyator <barthelemy.elizabeth@gmail.com>
2023-03-17 09:41:04 +0100

    protect player so they can't change colors

--- player.py
+++ player.py
@@ -1,5 +1,13 @@
 class Player:
 
     def __init__(self, color):
-        self.color = color  #TODO : protect, color of Player cannot change, merci Aline :-) 
-                            #TODO : idiotproof color can only be black or white
+        self.__color = color 
+                                    #TODO : idiotproof color can only be black or white
+
+    def __set_color(self, color):
+        raise AttributeError("Players cannot change sides")
+
+    def __get_color(self):
+        return self.__color
+    
+    color = property(__get_color, __set_color)
