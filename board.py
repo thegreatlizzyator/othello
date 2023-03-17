5cf6ef907946a2cf127e9dbbad157c020d940332
alexandre.appolaire <alexandre.appolaire@le-campus-numerique.fr>
2023-03-17 15:18:45 +0100

    correct the translate2XY function

--- board.py
+++ board.py
@@ -11,7 +11,7 @@ class Board:
         # self.allpawns = listofpawns # TODO: idiotproof check that list contains objects of class pawn
         self.liste_pos_y = ['a','b','c','d','e','f','g','h']
         self.liste_pos_Y = ['A','B','C','D','E','F','G','H']
-        self.liste_pos_x = [1,2,3,4,5,6,7,8] 
+        self.liste_pos_x = ['1','2','3','4','5','6','7','8'] 
         self.cells = np.empty((8,8), dtype=object)
         for x in range(0,8):
             for y in range(0,8):
@@ -56,20 +56,20 @@ class Board:
 
     
      
-        def translate2XY(self,AA): #translate A1 --> x,y
-            XY = list(AA)
-            y = XY[0] ; x = XY[1]
-            if y in self.liste_pos_y:
-                new_y = self.liste_pos_y.index(y)
-            elif y in self.liste_pos_Y:
-                new_y = self.liste_pos_Y.index(y)
-            else:
-                raise ValueError("La position indiquée n'existe pas sur le plateau")
-            if x in self.liste_pos_x:
-                new_x = self.liste_pos_x.index(x)
-            else:
-                raise ValueError("La position indiquée n'existe pas sur le plateau")
-            return (new_y,new_x)
+    def translate2XY(self,AA): #translate A1 --> x,y
+        YX = list(AA)
+        y = YX[0] ; x = YX[1]
+        if y in self.liste_pos_y:
+            new_y = self.liste_pos_y.index(y)
+        elif y in self.liste_pos_Y:
+            new_y = self.liste_pos_Y.index(y)
+        else:
+            raise ValueError("La position indiquée n'existe pas sur le plateau")
+        if x in self.liste_pos_x:
+            new_x = self.liste_pos_x.index(x)
+        else:
+            raise ValueError("La position indiquée n'existe pas sur le plateau")
+        return (new_y,new_x)
 
     def translate2A1(self,y,x): # translate x,y --> A1  
         if y in range (0,len(self.liste_pos_Y)) :
